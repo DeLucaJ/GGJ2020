a97b1d2a477b942391a20950459adff1dbf37bdd
Joseph DeLuca <delucaj96@gmail.com>
2020-02-02 14:19:35 -0800

    Audio and some Character Audio

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ink.Runtime;
 
 public class InventoryManager : MonoBehaviour
 {
+    public StoryManager storyManager;
+
+    private InkList inventory;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventory = new InkList("inventory", storyManager.story);
+        Debug.Log(inventory.Count);
     }
 
     // Update is called once per frame
